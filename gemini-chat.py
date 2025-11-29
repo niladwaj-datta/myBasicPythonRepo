c9c58c253c24a5f73b73a70a4bb239d9ef546877
niladwaj-datta <niladwaj.datta@gmail.com>
2025-11-29 19:34:52 +0530

    message updated

--- gemini-chat.py
+++ gemini-chat.py
@@ -4,7 +4,7 @@ API_KEY = "AIzaSyC4KKXT29wutK5pLQcsKmE-C-PXoWUF1ZI"
 genai.configure(api_key=API_KEY)
 model=genai.GenerativeModel("gemini-2.5-flash")
 chat = model.start_chat()
-print("Welcome to the Gemini-2.0-Flash chat!")
+print("Welcome to the Gemini-2.5-Flash chat!")
 while True:
     user_input = input("You: ")
     if user_input.lower() in ['exit', 'quit']:
